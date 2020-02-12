ed1a3ca3e75208f32e59047fe4563e893deaabc2
Ángel González <keisial@users.noreply.github.com>
2020-02-12 02:46:36 +0100

    Multiline header support
    Quite straight from sed FAQ 4.26
    http://sed.sourceforge.net/sedfaq4.html#s4.26

--- test-reorder-tags.sh
+++ test-reorder-tags.sh
@@ -19,3 +19,15 @@ test_case "Subject: [bar #12] hello world" "Subject: [foo #0] [bar #12] hello wo
 test_case "Subject: [bar #12] [foo #50] hello world" "Subject: [foo #50] [bar #12] hello world"
 test_case "Subject: [foo #5] [bar #2] hello world" "Subject: [foo #5] [bar #2] hello world"
 test_case "Subject: [bar #12] [baz #30] [foo #50] hello world" "Subject: [foo #50] [baz #30] [bar #12] hello world"
+test_case "Subject: hello
+ [bar #2]
+ world [foo #5]" "Subject: hello [foo #5] world [bar #2]"
+test_case "From: John
+ Smith <jsmith@example.com>
+Subject: hello
+ [bar #2]
+ world [foo #5]" "From: John
+ Smith <jsmith@example.com>
+Subject: hello [foo #5] world [bar #2]"
+test_case "Subject: [bar
+ #2][foo #5] test" "Subject: [foo #5][bar #2] test"
