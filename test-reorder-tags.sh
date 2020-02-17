9610075fd0781b43bf40d7c1df611a2d40e4c48a
Ángel González <keisial@users.noreply.github.com>
2020-02-17 04:48:47 +0100

    Allow adding extra filters

--- test-reorder-tags.sh
+++ test-reorder-tags.sh
@@ -6,7 +6,7 @@ REORDER_TAGS="$(dirname "$0")/reorder-tags"
 
 test_case() {
 	printf "Test %s... " "$1"
-	OUTPUT="$(echo "$1" | $REORDER_TAGS foo bar)"
+	OUTPUT="$(echo "$1" | $REORDER_TAGS ${3:-} foo bar)"
 	if [ "$OUTPUT" = "$2" ]; then
 	  echo Ok
 	else
@@ -34,3 +34,6 @@ test_case "Subject: [bar
 test_case "Subject:
  =?UTF-8?Q?=5Bbar_=2312=5D_=5Bfoo_=2350=5D_rfc2047?=" "Subject: =?UTF-8?Q?=5Bfoo_=2350=5D_=5Bbar_=2312=5D_rfc2047?="
 test_case "Subject: =?UTF-8?Q?=5Bbar #12=5D?= [foo #50]" "Subject: =?UTF-8?Q?[foo #50]?= =5Bbar #12=5D"  # Breaks if one tag is encoded but the other isn't
+test_case $'From: joe@doe.com\nSubject: [bar #2][foo #5]' $'From: joe@doe.com\nSubject: [foo #5][bar #2]'
+test_case $'From: joe@doe.com\nSubject: [bar #2][foo #5]' $'From: joe@doe.com\nSubject: [foo #5][bar #2]' "--filter /From:.*@doe\.com/"  # Filter if sent from Doe
+test_case $'From: joe@doe.com\nSubject: [bar #2][foo #5]' $'From: joe@doe.com\nSubject: [bar #2][foo #5]' "--filter /From:.*@smith\.com/"  # But here we do not filter, as it doesn't come from Smith
